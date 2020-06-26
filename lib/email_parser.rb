1b75694052da95d99034e80081c3b0263e3451e7
Bryce Whittington <brycew30@gmail.com>
2020-06-26 17:29:06 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,8 +1,14 @@
 class EmailAddressParser 
 attr_accessor :emails
 
-def initialize(emails)
-  @emails = emails
+ def initialize(emails)
+   @emails = emails
+ end
+ 
+ def parse
+   emails.split.collect do |address|
+     address.split(',')
+ end
 end
 #that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
